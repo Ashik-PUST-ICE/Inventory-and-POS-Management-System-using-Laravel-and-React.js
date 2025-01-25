3f5c75cf8906ce69801e2c9e460b95a23b7f819c
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2025-01-26 01:51:13 +0600

    work on the admin section footer, navbar and Sidebar section

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
-import Nav from './component/Nav';
+import Nav from './component/partials/Nav';
 import './assets/css/styles.css';
+import SideBar from './component/partials/SideBar';
+import Footer from './component/partials/Footer';
 
 const App = () => {
   return (
     <>
       <Nav />
+
+      <div id="layoutSidenav">
+
+        <SideBar/>
+
+        <div id="layoutSidenav_content">
+        <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Dashboard</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">Success Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+
+
+  
+                        </div>
+
+                    </div>
+        </main>
+
+        <Footer/>
+        </div>
+
+      </div>
     </>
   );
 };
